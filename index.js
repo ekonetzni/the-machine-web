4e534d2230d00ba9e8e2f02ea1d9bdc69474d706
Elliot Konetzni <ekonetzni@gmail.com>
2018-04-13 15:17:17 +0200

    Added body parser config and routes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const db = require('./db');
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  //.use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
+  .use(bodyParser.json({ type: '*/*' }))
+  .get('/images', db.getAllImages)
+  .get('/images/:id', db.getSingleImage)
+  .post('/image', db.createImage)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
